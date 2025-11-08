0e8c6e5f69112057b1d7ff35a6a2f8f604098ab5
AguuZ12 <165302493+AguuZ12@users.noreply.github.com>
2025-11-08 19:22:07 -0300

    Delete Final directory

--- Final/src/Prueba.java
+++
@@ -1,8 +0,0 @@
-public class Prueba {
-    public static void main(String[] args){
-       GestionProductos gestion = new GestionProductos();
-        gestion.muestraItem("Pala");
-       gestion.quitarStock("Pala",13);
-       gestion.muestraItem("Pala");
-    }
-}
